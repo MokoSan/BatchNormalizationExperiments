a2633b6ac69f6819ab1e2bb0109f801c2a980432
anmolsjoshi <anmolsjoshi@gmail.com>
2018-07-08 17:32:06 -0700

    Added models/layers.py with PoolHelper and LRN updated to Keras with Tensorflow Backend

---
+++ src/models/layers.py
@@ -0,0 +1,57 @@
+from tensorflow.python.keras.layers import Layer
+from tensorflow.python.keras import backend as K
+
+
+class PoolHelper(Layer):
+
+    def __init__(self, **kwargs):
+        super(PoolHelper, self).__init__(**kwargs)
+
+    def call(self, x, mask=None):
+        return x[:, :, 1:, 1:]
+
+    def get_config(self):
+        config = {}
+        base_config = super(PoolHelper, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class LRN(Layer):
+
+    def __init__(self, alpha=0.0001, k=1, beta=0.75, n=5, **kwargs):
+        self.alpha = alpha
+        self.k = k
+        self.beta = beta
+        self.n = n
+        super(LRN, self).__init__(**kwargs)
+
+    def call(self, x, batch_size, mask=None):
+        _, ch, r, c = x.shape
+        b = batch_size
+        half_n = int(self.n // 2)  # half the local region
+        # orig keras code
+        # input_sqr = T.sqr(x)  # square the input
+        input_sqr = K.square(x)  # square the input
+        # orig keras code
+        # extra_channels = T.alloc(0., b, ch + 2 * half_n, r,c)  # make an empty tensor with zero pads along channel dimension
+        # input_sqr = T.set_subtensor(extra_channels[:, half_n:half_n+ch, :, :],input_sqr) # set the center to be the squared input
+
+        extra_channels = K.zeros((b, int(ch) + 2 * half_n, r, c))
+        input_sqr = K.concatenate(
+            [extra_channels[:, :half_n, :, :], input_sqr, extra_channels[:, half_n + int(ch):, :, :]], axis=1)
+
+        scale = self.k  # offset for the scale
+        norm_alpha = self.alpha / self.n  # normalized alpha
+        for i in range(self.n):
+            scale += norm_alpha * input_sqr[:, i:i + int(ch), :, :]
+        scale = scale ** self.beta
+        x = x / scale
+        return x
+
+    def get_config(self):
+        config = {"alpha": self.alpha,
+                  "k": self.k,
+                  "beta": self.beta,
+                  "n": self.n}
+        base_config = super(LRN, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
