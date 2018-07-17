0a41e76eeb34a28d58f28842ee23aa284c61d28a
MokoSan <mukund.sharma92@gmail.com>
2018-07-17 18:07:18 -0400

    Added an OO approach to GoogleNet

---
+++ src/models/GoogleNetCopy_test.py
@@ -0,0 +1,6 @@
+from GoogleNetCopy import GoogLeNetContext, GoogLeNet
+
+if __name__ == '__main__':
+    context = GoogLeNetContext()
+    model   = GoogLeNet( context )
+    #model = GoogLeNet( context ) 
---
+++ src/models/GoogleNet_test.py
@@ -0,0 +1,4 @@
+from GoogleNet import GoogleNet 
+
+if __name__ == '__main__':
+    g = GoogleNet()
